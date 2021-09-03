jdasagui/semgrep

commit 24e8cabfb30f1f3e87416bb6c4439a52e584d35b

Optimize pattern $X (#3703)
* Optimize pattern $X

Fixes: bfc4f3da447 ("Revert `pattern: $X` optimization (#3478)")
Fixes: 32e8897523c ("Separate pattern: $X in anded patterns to a special field (#3435)")

test plan:

    % make test # tests included

    % semgrep-core -lang py -config \
          ~/semgrep/semgrep-core/tests/OTHER/rules/pattern-x-1.yaml \
          bench/django/input/django
      #^ now takes ~6 seconds (i.e., almost 10x faster)

* engine: Clean up Specialize_formula

test plan:
make test

* Emma's comments

semgrep-core/src/engine/Match_rules.ml
@@ -112,6 +112,7 @@ type env = {
   pattern_matches : id_to_match_results;
   (* used by metavariable-pattern to recursively call evaluate_formula *)
   file : Common.filename;
+  lazy_ast_and_errors : (G.program * Error_code.error stack) lazy_t;
   rule_id : R.rule_id;
   xlang : R.xlang;
   equivalences : Equivalence.equivalences;
@@ -262,8 +263,8 @@ let debug_semgrep config mini_rules equivalences file lang ast =
 (* Evaluating Semgrep patterns *)
 (*****************************************************************************)
 
-let matches_of_patterns config equivalences (file, xlang, lazy_ast_and_errors)
-    patterns =
+let matches_of_patterns ?range_filter config equivalences
+    (file, xlang, lazy_ast_and_errors) patterns =
   match xlang with
   | R.L (lang, _) ->
       let (ast, errors), parse_time =
@@ -282,7 +283,7 @@ let matches_of_patterns config equivalences (file, xlang, lazy_ast_and_errors)
             else
               ( Match_patterns.check
                   ~hook:(fun _ _ -> ())
-                  config mini_rules equivalences (file, lang, ast),
+                  ?range_filter config mini_rules equivalences (file, lang, ast),
                 errors ))
       in
       { RP.matches; errors; profiling = { RP.parse_time; match_time } }
@@ -836,11 +837,7 @@ and (evaluate_formula : env -> RM.t option -> S.sformula -> RM.t list) =
         match posrs @ posrs_inside with
         | [] -> (
             match opt_context with
-            | None ->
-                [
-                  S.match_selector ~err:"empty And; no positive terms in And"
-                    selector_opt;
-                ]
+            | None -> failwith "empty And; no positive terms in And"
             | Some r -> [ [ r ] ])
         | ps -> ps
       in
@@ -855,6 +852,9 @@ and (evaluate_formula : env -> RM.t option -> S.sformula -> RM.t list) =
                    RM.intersect_ranges env.config !debug_matches acc r)
                  res
           in
+
+          (* optimization of `pattern: $X` *)
+          let res = run_selector_on_ranges env selector_opt res in
 
           (* let's remove the negative ranges *)
           let res =
@@ -880,22 +880,44 @@ and (evaluate_formula : env -> RM.t option -> S.sformula -> RM.t list) =
            *  - distribute filter_range in intersect_range?
            * See https://github.com/returntocorp/semgrep/issues/2664
            *)
-          let res =
-            conds
-            |> List.fold_left (fun acc cond -> filter_ranges env acc cond) res
-          in
-          S.select_from_ranges env.file selector_opt res)
+          conds
+          |> List.fold_left (fun acc cond -> filter_ranges env acc cond) res)
   | S.Not _ -> failwith "Invalid Not; you can only negate inside an And"
   | S.Leaf (R.MetavarCond _) ->
       failwith "Invalid MetavarCond; you can MetavarCond only inside an And"
 
+and run_selector_on_ranges env selector_opt ranges =
+  match (selector_opt, ranges) with
+  | _, [] ->
+      (* Empty context, nothing to select from. *)
+      []
+  | None, ranges ->
+      (* Nothing to select. *)
+      ranges
+  | Some { S.pattern; pid; pstr; _ }, ranges ->
+      (* Find matches of `pattern` *but* only inside `ranges`, this prevents
+       * matching and allocations that are wasteful because they are going to
+       * be thrown away later when interesecting the results. *)
+      let range_filter (tok1, tok2) =
+        let r = Range.range_of_token_locations tok1 tok2 in
+        List.exists (fun rwm -> Range.( $<=$ ) r rwm.RM.r) ranges
+      in
+      let patterns = [ (pattern, pid, fst pstr) ] in
+      let res =
+        matches_of_patterns ~range_filter env.config env.equivalences
+          (env.file, env.xlang, env.lazy_ast_and_errors)
+          patterns
+      in
+      logger#info "run_selector_on_ranges: found %d matches"
+        (List.length res.matches);
+      res.matches
+      |> List.map RM.match_result_to_range
+      |> RM.intersect_ranges env.config !debug_matches ranges
+
 and matches_of_formula config equivs rule_id file_and_more lazy_content formula
     opt_context : RP.times RP.match_result * RM.ranges =
   let file, xlang, lazy_ast_and_errors = file_and_more in
-  let match_func =
-    matches_of_patterns config equivs (file, xlang, lazy_ast_and_errors)
-  in
-  let formula = S.formula_to_sformula match_func formula in
+  let formula = S.formula_to_sformula formula in
   let xpatterns = xpatterns_in_formula formula in
   let res =
     matches_of_xpatterns config equivs
@@ -911,6 +933,7 @@ and matches_of_formula config equivs rule_id file_and_more lazy_content formula
       config;
       pattern_matches = pattern_matches_per_id;
       file;
+      lazy_ast_and_errors;
       rule_id;
       xlang;
       equivalences = equivs;

semgrep-core/src/engine/Specialize_formula.ml
@@ -7,9 +7,9 @@ module RP = Report
 
 type selector = {
   mvar : M.mvar;
+  pattern : AST_generic.any;
   pid : int;
   pstr : string R.wrap;
-  lazy_matches : RP.times RP.match_result Lazy.t;
 }
 
 type sformula =
@@ -25,97 +25,56 @@ type sformula =
 (* Selecting methods *)
 (*****************************************************************************)
 
-(* this will be adjusted later in range_to_pattern_match_adjusted *)
-let fake_rule_id (id, str) =
-  { PM.id = string_of_int id; pattern_string = str; message = "" }
-
-let match_selector ?err:(msg = "no match") (sel_opt : selector option) :
-    RM.ranges =
-  let get_match (x : RP.times RP.match_result) = x.matches in
-  match sel_opt with
-  | None -> failwith msg
-  | Some selector ->
-      List.map RM.match_result_to_range
-        (get_match (Lazy.force selector.lazy_matches))
-
-let select_from_ranges file (sel_opt : selector option) (ranges : RM.ranges) :
-    RM.ranges =
-  let pattern_match_from_binding selector (mvar, mval) =
-    match Visitor_AST.range_of_any_opt (M.mvalue_to_any mval) with
-    | None -> None
-    | Some range_loc ->
-        Some
-          {
-            PM.rule_id = fake_rule_id (selector.pid, fst selector.pstr);
-            PM.file;
-            PM.range_loc;
-            PM.tokens = lazy (M.ii_of_mval mval);
-            PM.env = [ (mvar, mval) ];
-          }
-  in
-  let select_from_range range =
-    match sel_opt with
-    | None -> Some range
-    | Some selector -> (
-        match
-          List.find_opt
-            (fun (mvar, _mval) -> M.equal_mvar selector.mvar mvar)
-            range.RM.mvars
-        with
-        | None -> None
-        | Some binding ->
-            pattern_match_from_binding selector binding
-            |> Common.map_opt RM.match_result_to_range)
-  in
-  Common.map_filter select_from_range ranges
-
 let selector_equal s1 s2 = s1.mvar = s2.mvar
 
 (*****************************************************************************)
 (* Converter *)
 (*****************************************************************************)
 
-let selector_from_formula match_func f =
+let selector_from_formula f =
   match f with
   | R.Leaf (R.P ({ pat = Sem (pattern, _); pid; pstr }, None)) -> (
       match pattern with
       | G.E { e = G.N (G.Id ((mvar, _), _)); _ } ->
-          Some
-            {
-              mvar;
-              pid;
-              pstr;
-              lazy_matches = lazy (match_func [ (pattern, pid, fst pstr) ]);
-            }
+          Some { mvar; pattern; pid; pstr }
       | _ -> None)
   | _ -> None
 
-let formula_to_sformula match_func formula =
+let formula_to_sformula formula =
+  let rec remove_selectors (selector, acc) formulas =
+    match formulas with
+    | [] -> (selector, acc)
+    | x :: xs ->
+        let selector, acc =
+          match (selector, selector_from_formula x) with
+          | None, None -> (None, x :: acc)
+          | Some s, None -> (Some s, x :: acc)
+          | None, Some s -> (Some s, acc)
+          | Some s1, Some s2 when selector_equal s1 s2 -> (Some s1, acc)
+          | Some s1, Some _s2 ->
+              (* patterns:
+               * ...
+               * - pattern: $X
+               * - pattern: $Y
+               *)
+              (* TODO: Should we fail here or just reported as a warning? This
+               * is something to catch with the meta-checker. *)
+              (Some s1, x :: acc)
+        in
+        remove_selectors (selector, acc) xs
+  in
   let rec formula_to_sformula formula =
-    let rec remove_selectors (selector, acc) formulas =
-      match formulas with
-      | [] -> (selector, acc)
-      | x :: xs ->
-          let selector, acc =
-            match (selector, selector_from_formula match_func x) with
-            | None, None -> (None, x :: acc)
-            | Some s, None -> (Some s, x :: acc)
-            | None, Some s -> (Some s, acc)
-            | Some s1, Some s2 ->
-                if selector_equal s1 s2 then (Some s1, acc) else (None, [])
-            (* This will never be valid *)
-          in
-          remove_selectors (selector, acc) xs
-    in
-    let _convert_and_formulas fs =
-      (* TODO put back this function *)
-      let selector, fs = remove_selectors (None, []) fs in
-      (selector, List.map formula_to_sformula fs)
+    let convert_and_formulas fs =
+      let selector, fs' = remove_selectors (None, []) fs in
+      (* We only want a selector if there is something to select from. *)
+      match fs' with
+      | [] -> (None, List.map formula_to_sformula fs)
+      | _ :: _ -> (selector, List.rev_map formula_to_sformula fs')
     in
     (* Visit formula and convert *)
     match formula with
     | R.Leaf leaf -> Leaf leaf
-    | R.And (_, fs) -> And (None, List.map formula_to_sformula fs)
+    | R.And (_, fs) -> And (convert_and_formulas fs)
     | R.Or (_, fs) -> Or (List.map formula_to_sformula fs)
     | R.Not (_, f) -> Not (formula_to_sformula f)
   in

semgrep-core/src/engine/Specialize_formula.mli
@@ -6,15 +6,17 @@
 
 type selector = {
   mvar : Metavariable.mvar;
+  pattern : AST_generic.any;
   pid : int;
   pstr : string Rule.wrap;
-  (* lazy_matches necessary in case `$X` is the only pattern *)
-  lazy_matches : Report.times Report.match_result Lazy.t;
 }
 
 type sformula =
   | Leaf of Rule.leaf
   | And of (selector option * sformula list)
+      (** Invariant: [And (sel_opt, fs)] satisfies
+     * [not (Option.is_some sel_opt) || fs <> []], that is,
+     * we can only select from a non-empty context. *)
   | Or of sformula list
   | Not of sformula
 
@@ -22,28 +24,13 @@ type sformula =
 (* Selecting methods *)
 (*****************************************************************************)
 
-val fake_rule_id : int * string -> Pattern_match.rule_id
-
-val match_selector :
-  ?err:string -> selector option -> Range_with_metavars.ranges
-
-val select_from_ranges :
-  string ->
-  selector option ->
-  Range_with_metavars.ranges ->
-  Range_with_metavars.ranges
-
 val selector_equal : selector -> selector -> bool
 
 (*****************************************************************************)
 (* Converter *)
 (*****************************************************************************)
 
-val formula_to_sformula :
-  ((AST_generic.any * Rule.pattern_id * string) list ->
-  Report.times Report.match_result) ->
-  Rule.formula ->
-  sformula
+val formula_to_sformula : Rule.formula -> sformula
 
 (*****************************************************************************)
 (* Visitor *)

semgrep-core/src/engine/Match_patterns.ml
@@ -205,7 +205,15 @@ let must_analyze_statement_bloom_opti_failed pattern_strs
 (*****************************************************************************)
 
 (*s: function [[Semgrep_generic.check2]] *)
-let check2 ~hook config rules equivs (file, lang, ast) =
+(* [range_filter] is a predicate that defines "regions of interest" when matching
+ *   expressions, this is e.g. used for optimizing `pattern: $X`. Note that
+ *   traversing the Generic AST is generally fairly cheap, what could be more
+ *   expensive is to do the matching (due to combinatorics) and all the allocations
+ *   associatiated, especially when the pattern causaes a lot of matches. This
+ *   filter allows us to avoid all that expensive stuff when matching expressions
+ *   unless they fall in specific regions of the code.
+ *   See also docs for {!check} in Match_pattern.mli. *)
+let check2 ~hook range_filter config rules equivs (file, lang, ast) =
   logger#info "checking %s with %d mini rules" file (List.length rules);
 
   let rules =
@@ -287,28 +295,35 @@ let check2 ~hook config rules equivs (file, lang, ast) =
              *)
             !expr_rules
             |> List.iter (fun (pattern, _bf, rule, cache) ->
-                   let env = MG.empty_environment cache config in
-                   let matches_with_env = match_e_e rule pattern x env in
-                   if matches_with_env <> [] then
-                     (* Found a match *)
-                     matches_with_env
-                     |> List.iter (fun (env : MG.tin) ->
-                            let env = env.mv.full_env in
-                            match V.range_of_any_opt (E x) with
-                            | None ->
-                                (* TODO: Report a warning to the user? *)
-                                logger#error
-                                  "Cannot report match because we lack range \
-                                   info: %s"
-                                  (show_expr x);
-                                ()
-                            | Some range_loc ->
+                   match V.range_of_any_opt (E x) with
+                   | None ->
+                       (* TODO: Report a warning to the user? *)
+                       logger#error
+                         "Cannot report match because we lack range info: %s"
+                         (show_expr x);
+                       ()
+                   | Some range_loc when range_filter range_loc ->
+                       let env = MG.empty_environment cache config in
+                       let matches_with_env = match_e_e rule pattern x env in
+                       if matches_with_env <> [] then
+                         (* Found a match *)
+                         matches_with_env
+                         |> List.iter (fun (env : MG.tin) ->
+                                let env = env.mv.full_env in
                                 let tokens = lazy (V.ii_of_any (E x)) in
                                 let rule_id = rule_id_of_mini_rule rule in
                                 Common.push
                                   { PM.rule_id; file; env; range_loc; tokens }
                                   matches;
-                                hook env tokens));
+                                hook env tokens)
+                   | Some (start_loc, end_loc) ->
+                       logger#info
+                         "While matching pattern %s in file %s, we skipped \
+                          expression at %d:%d-%d:%d (outside any range of \
+                          interest)"
+                         rule.pattern_string start_loc.file start_loc.line
+                         start_loc.column end_loc.line end_loc.column;
+                       ());
             (* try the rules on subexpressions *)
             (* this can recurse to find nested matching inside the
              * matched code itself *)
@@ -472,8 +487,8 @@ let check2 ~hook config rules equivs (file, lang, ast) =
 (*e: function [[Semgrep_generic.check2]] *)
 
 (* TODO: cant use [@@profile] because it does not handle yet label params *)
-let check ~hook config a b c =
+let check ~hook ?(range_filter = fun _ -> true) config a b c =
   Common.profile_code "Semgrep_generic.check" (fun () ->
-      check2 ~hook config a b c)
+      check2 ~hook range_filter config a b c)
 
 (*e: semgrep/engine/Match_patterns.ml *)

semgrep-core/src/engine/Match_patterns.mli
@@ -3,11 +3,20 @@
 (*s: signature [[Semgrep_generic.check]] *)
 val check :
   hook:(Metavariable.bindings -> Parse_info.t list Lazy.t -> unit) ->
+  ?range_filter:(Parse_info.token_location * Parse_info.token_location -> bool) ->
   Config_semgrep.t ->
   Mini_rule.rules ->
   Equivalence.equivalences ->
   Common.filename * Lang.t * Target.t ->
   Pattern_match.t list
+(** Match mini-rules (patterns) against a target file.
+  *
+  * @param range_filter A predicate that defines "regions of interest" when matching
+  *    expressions. This is e.g. used for optimizing `pattern: $X` by filtering out
+  *    any matches that are not within the ranges of preceding `pattern-inside`s.
+  *    Typically you will check that the range of the expression is strictly inside
+  *    your area(s) of interest, no need to worry about sub-expressions, they will be
+  *    visited regardless. *)
 
 (*e: signature [[Semgrep_generic.check]] *)
 

semgrep-core/src/engine/Tainting_generic.ml
@@ -56,7 +56,7 @@ let any_in_ranges any ranges =
         (G.show_any any);
       false
   | Some (tok1, tok2) ->
-      let r = { Range.start = tok1.charpos; end_ = tok2.charpos } in
+      let r = Range.range_of_token_locations tok1 tok2 in
       List.exists (Range.( $<=$ ) r) ranges
 
 let ranges_of_pformula config equivs file_and_more rule_id pformula =

semgrep-core/tests/OTHER/rules/pattern-x-1.py
@@ -1,0 +1,7 @@
+def foo(x):
+  #ok:test
+  a = x + b
+  #ok:test
+  c = a + x
+  #ruleid:test
+  return (c + x)

semgrep-core/tests/OTHER/rules/pattern-x-1.yaml
@@ -1,0 +1,12 @@
+rules:
+- id: test
+  languages: [python]
+  patterns:
+  - pattern-inside: |
+      def $F($X, ...):
+          ...
+  - pattern-inside: |
+      return ...
+  - pattern: $X
+  message: Test
+  severity: ERROR

commit 4bcf2a6572ab60695c162ef133b803d33e43a2cd

core: engine: Fix crash "empty And; no positive terms in And"
We assumed that we only get a selector when there is a context to select
from. But this was not the case in the presence negative or conditional
pattern operators.

Fixes: 24e8cabfb30 ("Optimize pattern $X (#3703)")

test plan:
make test # test included

semgrep-core/src/engine/Match_rules.ml
@@ -187,18 +187,6 @@ let (range_to_pattern_match_adjusted : Rule.t -> RM.t -> Pattern_match.t) =
   (* rather than the original metavariables for the match                          *)
   { m with rule_id; env = range.mvars }
 
-(* return list of "positive" x list of Not x list of Conds *)
-let (split_and :
-      S.sformula list -> S.sformula list * S.sformula list * R.metavar_cond list)
-    =
- fun xs ->
-  xs
-  |> Common.partition_either3 (fun e ->
-         match e with
-         | S.Not f -> Middle3 f
-         | S.Leaf (R.MetavarCond (_, c)) -> Right3 c
-         | _ -> Left3 e)
-
 let lazy_force x = Lazy.force x [@@profiling]
 
 (*****************************************************************************)
@@ -816,9 +804,9 @@ and (evaluate_formula : env -> RM.t option -> S.sformula -> RM.t list) =
       |> List.map RM.match_result_to_range
       |> List.map (fun r -> { r with RM.kind })
   | S.Or xs -> xs |> List.map (evaluate_formula env opt_context) |> List.flatten
-  | S.And (selector_opt, xs) -> (
-      let pos, neg, conds = split_and xs in
-
+  | S.And
+      { selector_opt; positives = pos; negatives = neg; conditionals = conds }
+    -> (
       (* we now treat pattern: and pattern-inside: differently. We first
        * process the pattern: and then the pattern-inside.
        * This fixed only one mismatch in semgrep-rules.

semgrep-core/src/engine/Specialize_formula.ml
@@ -15,12 +15,19 @@ type selector = {
 
 type sformula =
   | Leaf of R.leaf
-  | And of (selector option * sformula list)
+  | And of sformula_and
   | Or of sformula list
   (* There are restrictions on where a Not can appear in a formula. It
    * should always be inside an And to be intersected with "positive" formula.
    *)
   | Not of sformula
+
+and sformula_and = {
+  selector_opt : selector option;
+  positives : sformula list;
+  negatives : sformula list;
+  conditionals : R.metavar_cond list;
+}
 [@@deriving show]
 
 (*****************************************************************************)
@@ -33,9 +40,20 @@ let selector_equal s1 s2 = s1.mvar = s2.mvar
 (* Converter *)
 (*****************************************************************************)
 
+(* return list of "positive" x list of Not x list of Conds *)
+let split_and :
+    R.formula list -> R.formula list * R.formula list * R.metavar_cond list =
+ fun xs ->
+  xs
+  |> Common.partition_either3 (fun e ->
+         match e with
+         | R.Not (_, f) -> Middle3 f
+         | R.Leaf (R.MetavarCond (_, c)) -> Right3 c
+         | _ -> Left3 e)
+
 let selector_from_formula f =
   match f with
-  | R.Leaf (R.P ({ pat = Sem (pattern, _); pid; pstr }, None)) -> (
+  | Leaf (R.P ({ pat = Sem (pattern, _); pid; pstr }, None)) -> (
       match pattern with
       | G.E { e = G.N (G.Id ((mvar, _), _)); _ } when MV.is_metavar_name mvar ->
           Some { mvar; pattern; pid; pstr }
@@ -66,19 +84,28 @@ let formula_to_sformula formula =
         remove_selectors (selector, acc) xs
   in
   let rec formula_to_sformula formula =
-    let convert_and_formulas fs =
-      let selector, fs' = remove_selectors (None, []) fs in
-      (* We only want a selector if there is something to select from. *)
-      match fs' with
-      | [] -> (None, List.map formula_to_sformula fs)
-      | _ :: _ -> (selector, List.rev_map formula_to_sformula fs')
-    in
     (* Visit formula and convert *)
     match formula with
     | R.Leaf leaf -> Leaf leaf
     | R.And (_, fs) -> And (convert_and_formulas fs)
     | R.Or (_, fs) -> Or (List.map formula_to_sformula fs)
     | R.Not (_, f) -> Not (formula_to_sformula f)
+  and convert_and_formulas fs =
+    let pos, neg, cond = split_and fs in
+    let pos = List.map formula_to_sformula pos in
+    let neg = List.map formula_to_sformula neg in
+    let sel, pos =
+      (* We only want a selector if there is something to select from. *)
+      match remove_selectors (None, []) pos with
+      | _, [] -> (None, pos)
+      | sel, pos -> (sel, pos)
+    in
+    {
+      selector_opt = sel;
+      positives = pos;
+      negatives = neg;
+      conditionals = cond;
+    }
   in
   formula_to_sformula formula
 
@@ -91,4 +118,7 @@ let rec visit_sformula f formula =
   | Leaf (P (p, i)) -> f p i
   | Leaf (MetavarCond _) -> ()
   | Not x -> visit_sformula f x
-  | Or xs | And (_, xs) -> xs |> List.iter (visit_sformula f)
+  | Or xs -> xs |> List.iter (visit_sformula f)
+  | And fand ->
+      fand.positives |> List.iter (visit_sformula f);
+      fand.negatives |> List.iter (visit_sformula f)

semgrep-core/src/engine/Specialize_formula.mli
@@ -14,13 +14,18 @@ type selector = {
 
 type sformula =
   | Leaf of Rule.leaf
-  | And of (selector option * sformula list)
-      (** Invariant: [And (sel_opt, fs)] satisfies
-     * [not (Option.is_some sel_opt) || fs <> []], that is,
-     * we can only select from a non-empty context.
-     *)
+  | And of sformula_and
   | Or of sformula list
   | Not of sformula
+
+and sformula_and = {
+  selector_opt : selector option;
+      (** Invariant: [not (Option.is_some selector_opt) || positives <> []]
+          that is, we can only select from a non-empty context. *)
+  positives : sformula list;
+  negatives : sformula list;
+  conditionals : Rule.metavar_cond list;
+}
 [@@deriving show]
 
 (*****************************************************************************)

semgrep-core/tests/OTHER/rules/pattern-x-bug.py
@@ -1,0 +1,2 @@
+def test(x):
+    return x

semgrep-core/tests/OTHER/rules/pattern-x-bug.yaml
@@ -1,0 +1,9 @@
+rules:
+  - id: test
+    languages:
+      - py
+    message: Match Found!
+    patterns:
+    - pattern: $X
+    - pattern-not: x
+    severity: WARNING